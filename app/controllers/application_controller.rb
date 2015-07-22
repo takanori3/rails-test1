b8883466d9c23879d2ccda95a415966303d978e9
niino3 <tn@fortissimo.co.jp>
2015-07-22 01:22:39 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def list
+    @books = Book.all
+  end
+  
+  def json
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+  # Post 
+    area = params[:q]
+    
+  #  if area.nil? then
+      area = 'Setagaya,jp'
+  #  end
+
+    json_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + area + '&units=metric'
+    
+  # 読み込んで
+    uri = URI.parse(json_url)
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+    
+    puts result
+  　@obj = result
+
+  end
+  
+end
